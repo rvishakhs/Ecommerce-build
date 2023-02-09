69df31fb1886a62e65d1fd58f88798ddabdefd79
rvishakhs <rvishakhs@gmail.com>
2023-02-09 23:55:26 +0000

    banner going on

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,11 +8,17 @@ import subimg4 from "../images/catbanner-04.jpg"
 function Banner() {
   return (
     <div className='grid grid-cols-4 px-4 py-4 gap-2'>
-        <div className='col-span-2'>
+        <div className='col-span-2 relative'>
             <img 
                 src={main1}
                 className="w-[620px] h-[425px] rounded-xl object-cover"
             />
+            <div className='absolute flex flex-col top-[15%] left-12'>
+                <h3 className='font-bold text-xl '>SUPERCHARGED FOR PROS</h3>
+                <h2>Special Sale</h2>
+                <h4>From $999.00 or $41.62/mo.</h4>
+                <h4>for 24 month.*</h4>
+            </div>
         </div>
         <div className=' flex col-span-2 flex-wrap gap-4'>
             <div className='grid space-x-2' >
