3147722da99c105671a206692c9a4f49cd8c5f2e
rvishakhs <rvishakhs@gmail.com>
2023-02-12 15:35:55 +0000

    product section finished

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import watch from "../images/watch.jpg"
+import watch from "../images/watch1.JPG"
+import watchalt from "../images/watch2.JPG"
 import StarRatings from 'react-star-ratings';
 import { MdOutlineCompareArrows } from "react-icons/md";
-import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEye, AiOutlineHeart } from "react-icons/ai";
 import { BsBagPlus } from "react-icons/bs";
 
 function Productcard() {
   return (
-    <div className='w-[250px] h-[320px] group relative gap-3 bg-white rounded-xl'>
-        <img 
-            src={watch}
-            className="w-[250px] h-[200px] rounded-t-xl object-cover"
-            alt='productimage'
-        />
+    <div className='w-[250px] h-[330px] group relative gap-3 bg-white rounded-xl my-2'>
+        <div className='w-[250px] group h-[220px]  py-2'>
+            <img 
+                src={watch}
+                className="w-[250px] group-hover:hidden h-[200px] rounded-t-xl object-contain"
+                alt='productimage'
+            />
+            <img 
+                src={watchalt}
+                className="w-[250px] hidden group-hover:block h-[200px] rounded-t-xl object-contain"
+                alt='productimage'
+            />
+            </div>
         <div className='space-y-1 px-3'>
             <p className='font-medium text-red-800 text-sm '>Harvells</p>
             <p className='font-bold text-black text-sm'>Harwells Watch collections</p>            
@@ -25,6 +33,11 @@ function Productcard() {
                 starSpacing="2px"
             />        
             <p className='font-bold text-gray-500 '> $ 800</p>
+        </div>
+        <div className='absolute flex items-center top-4 right-3'>
+            <AiOutlineHeart
+                className="w-6 h-6 cursor-pointer hover:scale-105"
+            />
         </div>
         <div className='absolute hidden  group-hover:block flex-col space-y-2 top-12 right-3 '>
             <MdOutlineCompareArrows className='w-6 h-6 cursor-pointer hover:scale-105'/>
