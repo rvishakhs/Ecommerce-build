4bc9b4480dd2d6f00bab9657601ff7ea7f8dbb66
rvishakhs <rvishakhs@gmail.com>
2023-02-17 23:58:01 +0000

    store section

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -5,7 +5,7 @@ import { SlHome } from "react-icons/sl";
 function Breadcrumb({tittle}) {
   return (
     <div className='py-2 px-4 flex gap-2 items-center'>
-        <SlHome className="w-4 h-4"/>
+        <SlHome className="w-4 h-4 text-gray-500" />
         <p className='font-medium text-sm text-gray-500'>
             <Link to="/">Home {'>'} </Link> {tittle}
         </p>
--- src/pages/Ourstore.jsx
+++ src/pages/Ourstore.jsx
@@ -7,6 +7,10 @@ function Ourstore() {
     <>
       <Meta head="Store | Ecomm"/>
       <Breadcrumb tittle="Our store" />
+      <div className='max-w-7xl grid grid-cols-2 md:grid-cols-7 lg:grid-cols-10 '>
+          <div className='col-span-2'></div>
+          <div className='col-span-7'></div>
+      </div>
     </>
   )
 }
