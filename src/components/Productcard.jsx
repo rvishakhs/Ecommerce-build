daa85f86a4202bd7262deac9c4d004ba30dd1556
rvishakhs <rvishakhs@gmail.com>
2023-02-19 20:54:57 +0000

    working on product page almost completed

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -11,15 +11,14 @@ function Productcard({grid}) {
     
     let location = useLocation();
     
-    
-    return (
+        return (
 
-        <div className={`${location.pathname === "/store" ? `col-span-${grid}  ${grid === 2 ? "flex flex-col" : "flex flex-row"}  bg-white rounded-xl my-2 ` 
+        <div className={`${location.pathname === "/store" ? `col-span-${grid}  ${grid === 2 ? "flex flex-col" : "flex flex-row"} relative  group bg-white rounded-xl my-2 ` 
         : "w-[250px] h-[330px] group relative gap-3 bg-white rounded-xl my-2"}`}>
         <div className={`${grid === 2 ? "w-[200px] group h-[200px]  py-2" : "w-[250px] group h-[220px]  py-2"}`}>
             <img 
                 src={watch}
-                className="w-[250px] group-hover:hidden h-[200px] rounded-t-xl object-contain"
+                className="w-[250px] group-hover:hidden h-[200px]  rounded-t-xl object-contain"
                 alt='productimage'
             />
             <img 
@@ -28,7 +27,7 @@ function Productcard({grid}) {
                 alt='productimage'
             />
             </div>
-        <div className={`${location.pathname === "/store" ? `pt-4 ${grid === 2 ? "px-2 py-2" : "px-3  py-2"}` : "space-y-1 px-3"} `}>
+        <div className={`${location.pathname === "/store" ? `max-w-[70%] pt-4 ${grid === 2 ? "px-2 py-2" : "pr-8  py-2"}` : "space-y-1 px-3"} `}>
             <p className='font-medium text-red-800 text-sm '>Harvells</p>
             <p className='font-bold text-black text-sm'>Harwells Watch collections</p>            
             <StarRatings
@@ -38,18 +37,19 @@ function Productcard({grid}) {
                 name='rating'
                 starDimension="18px"
                 starSpacing="2px"
-                />        
+                />     
+            <p className={`font-normal text-sm text-gray-500 text-justify py-2 ${grid === 10 ? "block" : "hidden"}`}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>       
             <p className='font-bold text-gray-500 '> $ 800</p>
         </div>
         <div className='absolute flex items-center top-4 right-3'>
             <AiOutlineHeart
-                className="w-6 h-6 cursor-pointer hover:scale-105"
+                className={`${grid === 2 ? "w-5 h-5 cursor-pointer hover:scale-105" : "w-6 h-6 cursor-pointer hover:scale-105"} `}
                 />
         </div>
         <div className='absolute hidden  group-hover:block flex-col space-y-2 top-12 right-3 '>
-            <MdOutlineCompareArrows className='w-6 h-6 cursor-pointer hover:scale-105'/>
-            <AiOutlineEye className='w-6 h-6 cursor-pointer hover:scale-105'/>
-            <BsBagPlus className='w-5 h-5 cursor-pointer hover:scale-105'/>
+            <MdOutlineCompareArrows className={` ${grid === 2 || grid === 3   ? "w-5 h-5 cursor-pointer hover:scale-105" : "w-6 h-6 cursor-pointer hover:scale-105"}`}/>
+            <AiOutlineEye className={` ${grid === 2 || grid === 3 ? "w-5 h-5 cursor-pointer hover:scale-105" : "w-6 h-6 cursor-pointer hover:scale-105"}`}/>
+            <BsBagPlus className={` ${grid === 2 || grid === 3 ? "w-5 h-5 cursor-pointer hover:scale-105" : "w-6 h-6 cursor-pointer hover:scale-105"}`}/>
         </div>
     </div> 
   )
