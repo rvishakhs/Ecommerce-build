4b279df8dd534a254c6351dc81af83d6add6ac6d
rvishakhs <rvishakhs@gmail.com>
2023-02-21 00:26:00 +0000

    compare page completed

---
+++ src/components/CompareProduct .jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import watch from "../images/watch2.JPG"
+import { AiOutlineClose } from "react-icons/ai";
+import ReactStars from 'react-rating-stars-component';
+
+function CompareProduct () {
+  return (
+    <div className='w-full col-span-1  md:col-span-2 px-2 py-2 bg-white rounded-xl'>
+        <div className='flex relative flex-col gap-1'>
+            <img 
+                src={watch}
+                alt="Compare1img"
+                className='w-full h-[200px] object-contain'
+            />
+            <AiOutlineClose className='w-5 h-5 absolute top-2 cursor-pointer hover:scale-110 right-3' />  
+            <p className='font-bold text-base text-black px-2'>Apple Watch Series 8</p>
+            <div className='divide-y-2'>
+                <div className='flex flex-row space-x-4 items-center py-2'>
+                    <p className='font-bold text-base text-black px-2'>$800</p>
+                    <ReactStars
+                        count={5}
+                        value={4}
+                        size={18}
+                        activeColor="#ffd700"
+                        edit={false}
+                        />
+                </div>
+                <p className='font-semibold text-gray-500 px-2 py-2'>Brand : <span className='px-6 text-black'>Apple</span></p>
+                <p className='font-semibold text-gray-500 px-2 py-2'>Type : <span className='px-6 text-black'>Smart Watch</span></p>
+                <p className='font-semibold text-gray-500 px-2 py-2'>Color :<span className='px-6 text-black'></span></p>
+                <p className='font-semibold text-gray-500 px-2 py-2'>Availability :<span className='px-6 text-black'>In Stock</span></p>
+                <p className='font-semibold text-gray-500 px-2 py-2'>Technical Details :<span className='px-6 text-black'></span></p>
+
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default CompareProduct 
--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
+import CompareProduct from '../components/CompareProduct '
 import Meta from '../components/Meta'
 
 function Compare() {
@@ -7,6 +8,17 @@ function Compare() {
     <>
         <Meta head="Compare | Ecomm"/>
         <Breadcrumb tittle="Compare" />
+        <main className='bg-gray-200 py-4'>
+            <div className='max-w-7xl mx-auto grid overflow-x-scroll scrollbar-hide grid-cols-2  gap-2 md:grid-cols-8 lg:grid-cols-10 '>
+
+                    <CompareProduct />
+                    <CompareProduct />
+                    <CompareProduct />
+                    <CompareProduct />
+                    <CompareProduct />
+
+            </div>
+        </main>
 
     </>
   )
